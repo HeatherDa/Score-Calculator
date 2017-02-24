f33a04e94061e8e5b03b9d6496e702e346e8c920
HeatherDa <crystal_tree@hotmail.com>
2017-02-24 11:01:17 -0600

    First commit

--- Score Calculator/Form1.Designer.cs
+++ Score Calculator/Form1.Designer.cs
@@ -28,12 +28,289 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAverage = new System.Windows.Forms.TextBox();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtLow = new System.Windows.Forms.TextBox();
+            this.txtHigh = new System.Windows.Forms.TextBox();
+            this.txtMedian = new System.Windows.Forms.TextBox();
+            this.txtQ1 = new System.Windows.Forms.TextBox();
+            this.txtQ2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score:";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(87, 12);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(40, 20);
+            this.txtScore.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(144, 10);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(56, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Score Total:";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(88, 40);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(39, 20);
+            this.txtTotal.TabIndex = 4;
+            this.txtTotal.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Score Count:";
+            // 
+            // txtCount
+            // 
+            this.txtCount.Location = new System.Drawing.Point(87, 74);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.ReadOnly = true;
+            this.txtCount.Size = new System.Drawing.Size(40, 20);
+            this.txtCount.TabIndex = 6;
+            this.txtCount.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Average:";
+            // 
+            // txtAverage
+            // 
+            this.txtAverage.Location = new System.Drawing.Point(88, 107);
+            this.txtAverage.Name = "txtAverage";
+            this.txtAverage.ReadOnly = true;
+            this.txtAverage.Size = new System.Drawing.Size(39, 20);
+            this.txtAverage.TabIndex = 8;
+            this.txtAverage.TabStop = false;
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(19, 287);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(94, 25);
+            this.btnDisplay.TabIndex = 9;
+            this.btnDisplay.Text = "Display Scores";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(129, 286);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 25);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear Scores";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(129, 324);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 25);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Low Score:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 176);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "High Score:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(2, 201);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = " Median Score:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(23, 228);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Quantile 1:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(23, 256);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(58, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Quantile 3:";
+            // 
+            // txtLow
+            // 
+            this.txtLow.Location = new System.Drawing.Point(88, 138);
+            this.txtLow.Name = "txtLow";
+            this.txtLow.ReadOnly = true;
+            this.txtLow.Size = new System.Drawing.Size(39, 20);
+            this.txtLow.TabIndex = 17;
+            this.txtLow.TabStop = false;
+            // 
+            // txtHigh
+            // 
+            this.txtHigh.Location = new System.Drawing.Point(88, 169);
+            this.txtHigh.Name = "txtHigh";
+            this.txtHigh.ReadOnly = true;
+            this.txtHigh.Size = new System.Drawing.Size(39, 20);
+            this.txtHigh.TabIndex = 18;
+            this.txtHigh.TabStop = false;
+            // 
+            // txtMedian
+            // 
+            this.txtMedian.Location = new System.Drawing.Point(87, 198);
+            this.txtMedian.Name = "txtMedian";
+            this.txtMedian.ReadOnly = true;
+            this.txtMedian.Size = new System.Drawing.Size(40, 20);
+            this.txtMedian.TabIndex = 19;
+            this.txtMedian.TabStop = false;
+            // 
+            // txtQ1
+            // 
+            this.txtQ1.Location = new System.Drawing.Point(87, 225);
+            this.txtQ1.Name = "txtQ1";
+            this.txtQ1.ReadOnly = true;
+            this.txtQ1.Size = new System.Drawing.Size(40, 20);
+            this.txtQ1.TabIndex = 20;
+            this.txtQ1.TabStop = false;
+            // 
+            // txtQ2
+            // 
+            this.txtQ2.Location = new System.Drawing.Point(87, 252);
+            this.txtQ2.Name = "txtQ2";
+            this.txtQ2.ReadOnly = true;
+            this.txtQ2.Size = new System.Drawing.Size(40, 20);
+            this.txtQ2.TabIndex = 21;
+            this.txtQ2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(222, 364);
+            this.Controls.Add(this.txtQ2);
+            this.Controls.Add(this.txtQ1);
+            this.Controls.Add(this.txtMedian);
+            this.Controls.Add(this.txtHigh);
+            this.Controls.Add(this.txtLow);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.txtAverage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Score Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAverage;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtLow;
+        private System.Windows.Forms.TextBox txtHigh;
+        private System.Windows.Forms.TextBox txtMedian;
+        private System.Windows.Forms.TextBox txtQ1;
+        private System.Windows.Forms.TextBox txtQ2;
     }
 }
 
--- Score Calculator/Form1.cs
+++ Score Calculator/Form1.cs
@@ -11,10 +11,87 @@ using System.Windows.Forms;
 namespace Score_Calculator
 {
     public partial class Form1 : Form
+
     {
+        //List of scores
+        List<int> scores = new List<int>();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            calculate();
+
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void calculate()//calculate and display results
+        {
+            if (dataValidation(txtScore))
+            {
+                scores.Add(Convert.ToInt16(txtScore.Text));
+            }
+        }
+        private bool dataValidation(TextBox t)
+        {
+            string name = "Score";
+
+            return (IsPresent(t, name) && IsNumber(t, name) && IsInRange(t, name, 0, 100));
+
+        }
+        public bool IsPresent(TextBox textBox, string name) //is there data in the text box?
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+        public bool IsNumber(TextBox textBox, string name) //is the data numeric?
+        {
+            decimal numdec = 0m;
+            if (Decimal.TryParse(textBox.Text, out numdec))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be a number.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+        }
+        public bool IsInRange(TextBox textBox, string name, decimal min, decimal max) //is the number in the right range?
+        {
+            decimal number = Convert.ToDecimal(textBox.Text);
+            if (number < min || number > max)
+            {
+                MessageBox.Show(name + " must be between " + min.ToString() + " and " + max.ToString() + ".", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+        
+        
+
     }
 }
