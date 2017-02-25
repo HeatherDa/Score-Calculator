39686f4f5c17dbbe6ed7a98e285052726e5105b8
HeatherDa <crystal_tree@hotmail.com>
2017-02-24 21:25:17 -0600

    Finnished

--- Score Calculator/Form1.Designer.cs
+++ Score Calculator/Form1.Designer.cs
@@ -43,19 +43,18 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
             this.txtLow = new System.Windows.Forms.TextBox();
             this.txtHigh = new System.Windows.Forms.TextBox();
             this.txtMedian = new System.Windows.Forms.TextBox();
-            this.txtQ1 = new System.Windows.Forms.TextBox();
-            this.txtQ2 = new System.Windows.Forms.TextBox();
+            this.txtPrevScore = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnUScores = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 19);
+            this.label1.Location = new System.Drawing.Point(52, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 0;
@@ -63,14 +62,14 @@
             // 
             // txtScore
             // 
-            this.txtScore.Location = new System.Drawing.Point(87, 12);
+            this.txtScore.Location = new System.Drawing.Point(96, 9);
             this.txtScore.Name = "txtScore";
             this.txtScore.Size = new System.Drawing.Size(40, 20);
             this.txtScore.TabIndex = 1;
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(144, 10);
+            this.btnAdd.Location = new System.Drawing.Point(153, 7);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(56, 23);
             this.btnAdd.TabIndex = 2;
@@ -81,7 +80,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 47);
+            this.label2.Location = new System.Drawing.Point(25, 84);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(65, 13);
             this.label2.TabIndex = 3;
@@ -89,7 +88,7 @@
             // 
             // txtTotal
             // 
-            this.txtTotal.Location = new System.Drawing.Point(88, 40);
+            this.txtTotal.Location = new System.Drawing.Point(97, 77);
             this.txtTotal.Name = "txtTotal";
             this.txtTotal.ReadOnly = true;
             this.txtTotal.Size = new System.Drawing.Size(39, 20);
@@ -99,7 +98,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 81);
+            this.label3.Location = new System.Drawing.Point(21, 118);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(69, 13);
             this.label3.TabIndex = 5;
@@ -107,7 +106,7 @@
             // 
             // txtCount
             // 
-            this.txtCount.Location = new System.Drawing.Point(87, 74);
+            this.txtCount.Location = new System.Drawing.Point(96, 111);
             this.txtCount.Name = "txtCount";
             this.txtCount.ReadOnly = true;
             this.txtCount.Size = new System.Drawing.Size(40, 20);
@@ -117,7 +116,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(31, 114);
+            this.label4.Location = new System.Drawing.Point(40, 151);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(50, 13);
             this.label4.TabIndex = 7;
@@ -125,7 +124,7 @@
             // 
             // txtAverage
             // 
-            this.txtAverage.Location = new System.Drawing.Point(88, 107);
+            this.txtAverage.Location = new System.Drawing.Point(97, 144);
             this.txtAverage.Name = "txtAverage";
             this.txtAverage.ReadOnly = true;
             this.txtAverage.Size = new System.Drawing.Size(39, 20);
@@ -134,7 +133,7 @@
             // 
             // btnDisplay
             // 
-            this.btnDisplay.Location = new System.Drawing.Point(19, 287);
+            this.btnDisplay.Location = new System.Drawing.Point(15, 265);
             this.btnDisplay.Name = "btnDisplay";
             this.btnDisplay.Size = new System.Drawing.Size(94, 25);
             this.btnDisplay.TabIndex = 9;
@@ -144,7 +143,7 @@
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(129, 286);
+            this.btnClear.Location = new System.Drawing.Point(125, 264);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(75, 25);
             this.btnClear.TabIndex = 10;
@@ -154,7 +153,8 @@
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(129, 324);
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(125, 302);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 25);
             this.btnExit.TabIndex = 11;
@@ -165,7 +165,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(20, 145);
+            this.label5.Location = new System.Drawing.Point(29, 182);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(61, 13);
             this.label5.TabIndex = 12;
@@ -174,7 +174,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(18, 176);
+            this.label6.Location = new System.Drawing.Point(27, 213);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(63, 13);
             this.label6.TabIndex = 13;
@@ -183,33 +183,15 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(2, 201);
+            this.label7.Location = new System.Drawing.Point(11, 238);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(79, 13);
             this.label7.TabIndex = 14;
             this.label7.Text = " Median Score:";
             // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(23, 228);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(58, 13);
-            this.label8.TabIndex = 15;
-            this.label8.Text = "Quantile 1:";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(23, 256);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(58, 13);
-            this.label9.TabIndex = 16;
-            this.label9.Text = "Quantile 3:";
-            // 
             // txtLow
             // 
-            this.txtLow.Location = new System.Drawing.Point(88, 138);
+            this.txtLow.Location = new System.Drawing.Point(97, 175);
             this.txtLow.Name = "txtLow";
             this.txtLow.ReadOnly = true;
             this.txtLow.Size = new System.Drawing.Size(39, 20);
@@ -218,7 +200,7 @@
             // 
             // txtHigh
             // 
-            this.txtHigh.Location = new System.Drawing.Point(88, 169);
+            this.txtHigh.Location = new System.Drawing.Point(97, 206);
             this.txtHigh.Name = "txtHigh";
             this.txtHigh.ReadOnly = true;
             this.txtHigh.Size = new System.Drawing.Size(39, 20);
@@ -227,30 +209,40 @@
             // 
             // txtMedian
             // 
-            this.txtMedian.Location = new System.Drawing.Point(87, 198);
+            this.txtMedian.Location = new System.Drawing.Point(96, 235);
             this.txtMedian.Name = "txtMedian";
             this.txtMedian.ReadOnly = true;
             this.txtMedian.Size = new System.Drawing.Size(40, 20);
             this.txtMedian.TabIndex = 19;
             this.txtMedian.TabStop = false;
             // 
-            // txtQ1
+            // txtPrevScore
             // 
-            this.txtQ1.Location = new System.Drawing.Point(87, 225);
-            this.txtQ1.Name = "txtQ1";
-            this.txtQ1.ReadOnly = true;
-            this.txtQ1.Size = new System.Drawing.Size(40, 20);
-            this.txtQ1.TabIndex = 20;
-            this.txtQ1.TabStop = false;
+            this.txtPrevScore.Location = new System.Drawing.Point(97, 44);
+            this.txtPrevScore.Name = "txtPrevScore";
+            this.txtPrevScore.ReadOnly = true;
+            this.txtPrevScore.Size = new System.Drawing.Size(39, 20);
+            this.txtPrevScore.TabIndex = 21;
+            this.txtPrevScore.TabStop = false;
             // 
-            // txtQ2
+            // label8
             // 
-            this.txtQ2.Location = new System.Drawing.Point(87, 252);
-            this.txtQ2.Name = "txtQ2";
-            this.txtQ2.ReadOnly = true;
-            this.txtQ2.Size = new System.Drawing.Size(40, 20);
-            this.txtQ2.TabIndex = 21;
-            this.txtQ2.TabStop = false;
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(8, 51);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(82, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Previous Score:";
+            // 
+            // btnUScores
+            // 
+            this.btnUScores.Location = new System.Drawing.Point(15, 302);
+            this.btnUScores.Name = "btnUScores";
+            this.btnUScores.Size = new System.Drawing.Size(94, 23);
+            this.btnUScores.TabIndex = 22;
+            this.btnUScores.Text = "Unsorted Scores";
+            this.btnUScores.UseVisualStyleBackColor = true;
+            this.btnUScores.Click += new System.EventHandler(this.btnUScores_Click);
             // 
             // Form1
             // 
@@ -258,14 +250,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnExit;
-            this.ClientSize = new System.Drawing.Size(222, 364);
-            this.Controls.Add(this.txtQ2);
-            this.Controls.Add(this.txtQ1);
+            this.ClientSize = new System.Drawing.Size(222, 338);
+            this.Controls.Add(this.btnUScores);
+            this.Controls.Add(this.txtPrevScore);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.txtMedian);
             this.Controls.Add(this.txtHigh);
             this.Controls.Add(this.txtLow);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -305,13 +296,12 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
         private System.Windows.Forms.TextBox txtLow;
         private System.Windows.Forms.TextBox txtHigh;
         private System.Windows.Forms.TextBox txtMedian;
-        private System.Windows.Forms.TextBox txtQ1;
-        private System.Windows.Forms.TextBox txtQ2;
+        private System.Windows.Forms.TextBox txtPrevScore;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnUScores;
     }
 }
 
--- Score Calculator/Form1.cs
+++ Score Calculator/Form1.cs
@@ -15,6 +15,7 @@ namespace Score_Calculator
     {
         //List of scores
         List<int> scores = new List<int>();
+        List<int> sortedScores = new List<int>();
 
         public Form1()
         {
@@ -24,22 +25,29 @@ namespace Score_Calculator
         private void btnAdd_Click(object sender, EventArgs e)
         {
             calculate();
+            txtScore.Clear();//make space for next score
+            txtScore.Focus();//ready for next score
 
         }
 
-        private void btnDisplay_Click(object sender, EventArgs e)
+        private void btnDisplay_Click(object sender, EventArgs e)//shows sorted data
         {
             string output="";
-            List<int> sortedScores = new List<int>();
-            sortedScores = scores;
-            scores.Sort();
+            foreach (int num in sortedScores)
+            {
+                output = output + num + "\n";
+            }
+            MessageBox.Show(output);
+        }
+        private void btnUScores_Click(object sender, EventArgs e)//shows unsorted data
+        {
+            string output = "";
             foreach (int num in scores)
             {
                 output = output + num + "\n";
             }
             MessageBox.Show(output);
         }
-
         private void btnClear_Click(object sender, EventArgs e)//clear all values
         {
             txtAverage.Clear();
@@ -47,11 +55,12 @@ namespace Score_Calculator
             txtHigh.Clear();
             txtLow.Clear();
             txtMedian.Clear();
-            txtQ1.Clear();
-            txtQ2.Clear();
             txtScore.Clear();
             txtTotal.Clear();
+            txtPrevScore.Clear();
             scores.Clear();
+            sortedScores.Clear();
+            txtScore.Focus();
         }
 
         private void btnExit_Click(object sender, EventArgs e)//close the program
@@ -60,20 +69,35 @@ namespace Score_Calculator
         }
         private void calculate()//calculate and display results
         {
-            if (dataValidation(txtScore))//if data validation returns true
+            try
             {
-                scores.Add(Convert.ToInt16(txtScore.Text));
-                decimal average = 0;//average score
-                decimal count = 0;//total scores
-                decimal total = 0;//total of scores
-                foreach (Int16 score in scores)
+                if (dataValidation(txtScore))//if data validation returns true
                 {
-                    total = +score;
-                    count++;
+                    scores.Add(Convert.ToInt16(txtScore.Text));
+                    sortedScores.Add(Convert.ToInt16(txtScore.Text));
+                    sortedScores.Sort();//sort scores and make sure variable is updated
+
+                    decimal total = 0;//total of scores
+                    foreach (Int16 score in scores)
+                    {
+                        total = total + score;//could maybe do without loop...
+                    }
+                    txtTotal.Text = Convert.ToString(total);//get and set total value of scores
+                    txtCount.Text = Convert.ToString(scores.Count());//get and set count of scores
+                    txtAverage.Text = Convert.ToString(total/scores.Count());//calculate and set Average score
+                    txtLow.Text = Convert.ToString(scores.Min());//calcultate and set Low score
+                    txtHigh.Text = Convert.ToString(scores.Max());//calculate and set High score
+                    calcMedian(); //set median
+
+                    txtPrevScore.Text = txtScore.Text; //so that user can see which number they just entered
                 }
-                average = total / count;
-
-
+            }catch(FormatException)
+            {
+                MessageBox.Show("There is a format exception.", "Exceptions");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("There is some kind of exception.", "Exception");
             }
         }
         private bool dataValidation(TextBox t)//data validation
@@ -119,7 +143,37 @@ namespace Score_Calculator
             return true;
         }
         
-        
+        private void calcMedian()//calculate median
+        {
+            int count = scores.Count();
+            decimal median = 0m;
+            int indexMedian = 0;
+            int index1 = 0;
+            int index2 = 0;
 
+            if ((count % 2 != 0)&&(scores.Count!=1))//ie if it's odd
+            {
+                indexMedian = (count + 1) / 2;
+                indexMedian = indexMedian - 1;//adjusting for index starting at 0
+                median = sortedScores[indexMedian];
+            }
+            else if (scores.Count == 1) { txtAverage.Text = "NA";  median=scores[0]; }//median is the only score available if there is only one score, Average is not applicable to a single score
+            else if (scores.Count ==2) { median = (Convert.ToDecimal(txtAverage.Text)); }//median is the same as the average if there are only two scores.
+            else//if it's even
+            {
+                indexMedian = count / 2;
+                indexMedian = indexMedian - 1;//adjusting for index starting at 0
+                index1 = sortedScores[indexMedian];
+                index2 = sortedScores[indexMedian+1];
+                decimal indexes = index1 + index2;
+                median = indexes / 2; //average of the two middle numbers
+            }
+
+            Math.Round(median,2);
+            txtMedian.Text = Convert.ToString(median);//set median text box with calculated value
+
+
+        }
+       
     }
 }
